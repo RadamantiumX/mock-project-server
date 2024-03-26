f6ad11826f57772ba68fee731df5cde5dbb98172
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-03-25 23:30:26 -0300

    adding new interface

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,5 +1,12 @@
-export interface UserInput{
+export interface AuthInput{
     nickname: string;
     email: string;
     password: string;
+}
+
+export interface PostInput{
+    content: string;
+    authorId: number;
+    videoId: string;
+    postId: number;
 }
