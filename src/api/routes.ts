b6fba7edc3c2903665deb3a796746bb1d9e1de57
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-27 00:20:15 -0300

    adding rout and test api

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from '../routers/auth.router';
 import userRouter from '../routers/user.router';
+import postRouter from '../routers/post.router';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
+router.use('/posts', postRouter)
 
 export default router;
