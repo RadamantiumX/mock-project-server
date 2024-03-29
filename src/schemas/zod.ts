6655e94a23fd76f93a6e13caca3c3cfba460ac58
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-03-28 22:38:54 -0300

    setting new validations into zod file

--- src/schemas/zod.ts
+++ src/schemas/zod.ts
@@ -3,15 +3,35 @@ import { AuthInput } from 'types'
 import { PostInput } from 'types'
 import { ResponsePostInput } from 'types'
 
+// Minimum 8 characters, at least one uppercase letter,
+// one lowercase letter, one number and one special character
+const passwordValidation = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+)
+
 const userSchema = z.object({
     nickname: z.string({
         invalid_type_error: 'The nickname must be a valid alphanumeric characters',
         required_error: 'The nickname is required'
-    }).min(5),
-    email: z.string().email(),
-    password: z.string().min(6).max(30),
-    
-})
+    }).min(5,{
+        message: 'The nickname must be larger than 5 characters minimum.'
+    }),
+    email: z.string().email({
+        message: 'Please entry a valida email address'
+    }),
+    password: z.string().min(6).max(30).regex(passwordValidation, {
+        message: 'Your password is not valid'
+    }),
+    confirmPassword: z.string().min(6).max(30)
+   
+}).refine((values) =>{
+        return values.password === values.confirmPassword
+    },
+    {
+        message: "Password must match!",
+        path: ["confirmPassword"]
+    }
+)
 const responsePostSchema = z.object({
     content: z.string({
         required_error: 'Empty content'
@@ -29,7 +49,7 @@ const postSchema = z.object({
     videoId: z.string()
 })
 
-export function validatePostSchema(input: AuthInput){
+export function validatePostSchema(input: PostInput){
     return responsePostSchema.safeParse(input)
 }
 
@@ -37,6 +57,6 @@ export function validateResponsePostSchema(input: ResponsePostInput){
     return postSchema.safeParse(input)
 }
 
-export function validateUserSchema(input: PostInput){
+export function validateUserSchema(input: AuthInput){
     return userSchema.safeParse(input)
 }
